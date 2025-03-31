0f928908c52fda73d8210cde1a4cbb8126c2dd62
frankpagan <frank@cocreate.app>
2025-03-31 07:08:51 -0600

    fix: update observer observe param to type and and attributeName to attributeFilter

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-import observer from '@cocreate/observer';
-import position from './position'
-import './index.css'
+import observer from "@cocreate/observer";
+import position from "./position";
+import "./index.css";
 
 function init() {
-    let elements = document.querySelectorAll('[moveable], [positionable]');
-    initElements(elements)
+	let elements = document.querySelectorAll("[moveable], [positionable]");
+	initElements(elements);
 }
 
 function initElements(elements) {
-    for (let element of elements)
-        initElement(element)
+	for (let element of elements) initElement(element);
 }
 
 function initElement(element) {
-    element.position = new position(element, element.parentElement);
+	element.position = new position(element, element.parentElement);
 }
 
 observer.init({
-    name: "positionAddedNodes",
-    observe: ['addedNodes'],
-    selector: '[moveable], [positionable]',
-    callback: mutation => {
-        initElement(mutation.target);
-    }
+	name: "positionAddedNodes",
+	types: ["addedNodes"],
+	selector: "[moveable], [positionable]",
+	callback: (mutation) => {
+		initElement(mutation.target);
+	}
 });
 
 observer.init({
-    name: "positionAttributes",
-    observe: ['attributes'],
-    attributeName: ['moveable', 'positionable'],
-    callback: mutation => {
-        initElement(mutation.target);
-    }
+	name: "positionAttributes",
+	types: ["attributes"],
+	attributeFilter: ["moveable", "positionable"],
+	callback: (mutation) => {
+		initElement(mutation.target);
+	}
 });
 
-init()
+init();
 
 export default position;
